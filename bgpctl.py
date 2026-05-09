13a2499635521a05126f410d8101b1f87641ea2b
xyzmean <litefros@yandex.ru>
2026-05-09 18:08:39 +0000

    Simplify BGP export: use 'export all' — next_hop is automatic in same subnet

--- bgpctl.py
+++ bgpctl.py
@@ -390,15 +390,7 @@ protocol bgp client {{
     neighbor {neighbor_ip} as {neighbor_as};
     multihop;
     hold time 240;
-    ipv4 {{
-        import none;
-        export filter {{
-            if source = RTS_STATIC then {{
-                bgp_next_hop = {local_ip};
-                accept;
-            }}
-        }};
-    }};
+    ipv4 {{ import none; export all; }};
 }}
 """
 
